bf3d38993c79d01bd14b75560f561eff83a8ff3a
nocount <wilsonburchenal@gmail.com>
2020-01-28 07:41:51 +0100

    Adding basic pre processing to get ready for model

--- good_good_ai_shit/data.py
+++ good_good_ai_shit/data.py
@@ -1,8 +1,12 @@
 # Export timeseries data for a single symbol to csv for later processing
 import config
 import pandas as pd
+import numpy as np
+from sklearn import preprocessing
 from alpha_vantage.timeseries import TimeSeries
 
+# Amount of days to look back when predicting the next days value
+history_points = 50
 
 def export_dataset(symbol):
     ts = TimeSeries(key=config.ALPHA_VANTAGE_API_KEY, output_format='pandas')
@@ -13,9 +17,32 @@ def export_dataset(symbol):
 
 def process_dataset(path):
     data = pd.read_csv(path)
-    return data
+    data = data.drop(0, axis=0)
+    data = data.drop('date', axis=1)
+
+    data_normalizer = preprocessing.MinMaxScaler()
+    data_normalized = data_normalizer.fit_transform(data)
+
+    # Calculate histories
+    ohlcv_histories_normalized = np.array(
+        [data_normalized[i:i + history_points].copy() for i in range(len(data_normalized) - history_points)])
+    next_day_open_values_normalized = np.array(
+        [data_normalized[:, 0][i + history_points].copy() for i in range(len(data_normalized) - history_points)])
+    next_day_open_values_normalized = np.expand_dims(next_day_open_values_normalized, -1)
+
+    next_day_open_values = np.array([data[:, 0][i + history_points].copy() for i in range(len(data) - history_points)])
+    next_day_open_values = np.expand_dims(next_day_open_values, -1)
+
+    y_normalizer = preprocessing.MinMaxScaler()
+    y_normalizer.fit(next_day_open_values)
+    
+    # TODO: need to include tech indicators here
+    
+    return ohlcv_histories_normalized, next_day_open_values_normalized, next_day_open_values, y_normalizer
     # do some other shit
 
 
 if __name__ == '__main__':
-    export_dataset('TSLA')
+    export_dataset('AAPL')
+    # data = process_dataset('TSLA_daily.csv')
+    # print(data)
